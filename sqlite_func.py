2e6d1f9acd88aa04bde4c94f7fc40060be33ec6a
met4lwar <98017704+metalw4rrior@users.noreply.github.com>
2023-05-23 23:08:11 +0500

    ввод даты в строку

--- sqlite_func.py
+++ sqlite_func.py
@@ -6,11 +6,6 @@ async def db_start():
     cur.execute("CREATE TABLE IF NOT EXISTS curators(curator_id INTEGER PRIMARY KEY, curator_fio TEXT, chat_id TEXT, password TEXT)")
     db.commit()
 
-# async def curators_values(chat_id):
-#     chat_id = str(chat_id)
-#     cur.execute = ("""INSERT INTO curators(chat_id) VALUES (?) """)
-#     # cur.execute(" INSERT INTO curators SET chat_id = (?)",(chat_id))
-#     db.commit()
 
 async def edit_profile(password, chat_id):
     password = str(password)
@@ -32,7 +27,7 @@ async def password_cheker(password):
         return True
 
 # Я думаю, можно назвать это говнокодом
-async def in_dbase(group_name, disrespectful_reason, valid_reason, disease_reason, present, user_id, date_of_report):
+async def in_dbase(group_name, disrespectful_reason, valid_reason, disease_reason, present, user_id, date_1):
     curator_id = str(cur.execute(f"SELECT curator_id FROM curators WHERE chat_id='{user_id}'").fetchone())
     delete = {ord('(') : None, ord(')') : None, ord(',') : None}
     curator_id = curator_id.translate(delete)
@@ -43,7 +38,7 @@ async def in_dbase(group_name, disrespectful_reason, valid_reason, disease_reaso
     # print(group_id, curator_id, date_of_report, valid_reason, disrespectful_reason, disease_reason, present)
     cur.execute(f"""INSERT INTO attendance_report 
     (group_id, curator_id, date_of_report, valid_reason, disrespectful_reason, disease_reason, who_is_present)
-    VALUES ({group_id}, {curator_id}, {date_of_report}, {valid_reason}, {disrespectful_reason}, {disease_reason}, {present})""")
+    VALUES ({group_id}, {curator_id}, {str(date_1)}, {valid_reason}, {disrespectful_reason}, {disease_reason}, {present})""")
     db.commit()
 
 # Функция, которая выводит группы
@@ -54,7 +49,8 @@ async def get_unoccupied_groups(user_id):
     info = str(cur.execute(f'SELECT group_name FROM groups WHERE curator_id="{curator_id}"').fetchall())
     delete = {ord('[') : None, ord(']') : None, ord('(') : None, ord(')') : None, ord(',') : None, ord("'") : None}
     info = info.translate(delete).split()
-    info = [info[i] + ' ' + info[i+1] for i in range(0, len(info), 2)]
+    if len(info)>1:
+        info = [info[i] + ' ' + info[i+1] for i in range(0, len(info), 2)]
     return info
 
 
